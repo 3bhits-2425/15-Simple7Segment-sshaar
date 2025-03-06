781fd34d77b728a982fd2e9e351866f5c90ab2cb
Sham Shaar <130391798+shaarsham@users.noreply.github.com>
2025-03-06 08:58:10 +0100

    first Push

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,143 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Manager : MonoBehaviour
+{
+    [SerializeField] private GameObject siebenSigment;
+    [SerializeField] private Button startButton;
+    private GameObject[] fluegel;
+    private Vector3[] startPos;
+    private bool isOn = false;
+
+    void Start()
+    {
+        fluegel = GameObject.FindGameObjectsWithTag("Fluegel");
+        startPos = new Vector3[fluegel.Length];
+        for (int i = 0; i < fluegel.Length; i++)
+        {
+            startPos[i] = fluegel[i].transform.position;
+        }
+    }
+
+    void Update()
+    {
+        if (isOn) 
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                ResetPos();
+                On(2);
+                On(7);
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                ResetPos();
+                On(0);
+                On(2);
+                On(3);
+                On(4);
+                On(6);
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                ResetPos();
+                On(0);
+                On(2);
+                On(3);
+                On(5);
+                On(6);
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha4))
+            {
+                ResetPos();
+                On(1);
+                On(2);
+                On(3);
+                On(5);
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha5))
+            {
+                ResetPos();
+                On(0);
+                On(1);
+                On(3);
+                On(5);
+                On(6);
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha6))
+            {
+                ResetPos();
+                On(0);
+                On(1);
+                On(3);
+                On(4);
+                On(5);
+                On(6);
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha7))
+            {
+                ResetPos();
+                On(0);
+                On(2);
+                On(5);
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha8))
+            {
+                ResetPos();
+                On(0);
+                On(1);
+                On(2);
+                On(3);
+                On(4);
+                On(5);
+                On(6);
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha9))
+            {
+                ResetPos();
+                On(0);
+                On(1);
+                On(2);
+                On(3);
+                On(5);
+                On(6);
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha0))
+            {
+                ResetPos();
+                On(0);
+                On(1);
+                On(2);
+                On(4);
+                On(5);
+                On(6);
+            }
+        }
+    }
+
+    public void StartSigment()
+    {
+        if (!isOn)
+        {
+            siebenSigment.transform.Rotate(0, 180, 0);
+            Rigidbody rb = siebenSigment.GetComponent<Rigidbody>();
+            rb.isKinematic = true;  
+            isOn = true;
+            startButton.interactable = false;
+        }
+        
+    }
+
+    private void On(int i)
+    {
+        fluegel[i].transform.position = startPos[i] + new Vector3(0, 0, 3);
+
+    }
+
+    private void ResetPos()
+    {
+        for (int i = 0; i < fluegel.Length; i++) {
+            fluegel[i].transform.position = startPos[i];
+        }   
+    }
+}
